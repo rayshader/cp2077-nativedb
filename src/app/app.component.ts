f440d356fce6253fc26dfbc7e80e5de30be03c4b
Louis Poirier <contact@louispoirier.io>
2023-12-02 16:36:45 +0100

    feat: add stylesheet mixins to theme application.
    Load icons and themes at application startup. Listen for theme mode changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, Renderer2} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {IconsService} from "../shared/services/icons.service";
+import {HttpClientModule} from "@angular/common/http";
+import {Theme, ThemeService} from "../shared/services/theme.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly themeS: Subscription;
+
+  constructor(private readonly iconsService: IconsService,
+              private readonly themeService: ThemeService,
+              private readonly renderer: Renderer2) {
+    this.themeS = this.themeService.onThemeChanged.subscribe(this.onThemeChanged.bind(this));
+  }
+
+  ngOnInit(): void {
+    this.iconsService.load();
+    this.themeService.load();
+  }
+
+  ngOnDestroy(): void {
+    this.themeS.unsubscribe();
+  }
+
+  private onThemeChanged(theme: Theme): void {
+    if (theme === 'light-theme') {
+      this.renderer.removeClass(document.body, 'dark-theme');
+    } else {
+      this.renderer.addClass(document.body, theme);
+    }
+  }
 }
