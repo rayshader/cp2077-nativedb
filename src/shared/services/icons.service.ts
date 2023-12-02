4c5eae0a2c387890c06cb51fdfd67b71fd9bee88
Louis Poirier <contact@louispoirier.io>
2023-12-02 14:52:57 +0100

    feat: add Icons service with assets.
    Load SVG badge icons to be used with <mat-icon>.

---
+++ src/shared/services/icons.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IconsService {
+  private readonly icons: string[] = [
+    // Nodes
+    'enum', 'bitfield', 'class', 'struct', 'function',
+
+    // Visibility
+    'scope',
+
+    // Flags
+    'abstract', 'callback', 'const', 'final', 'importonly', 'native', 'quest', 'replicated', 'static'
+  ];
+
+  constructor(private readonly iconRegistry: MatIconRegistry,
+              private readonly sanitizer: DomSanitizer) {
+  }
+
+  load(): void {
+    for (const icon of this.icons) {
+      this.iconRegistry.addSvgIcon(
+        icon,
+        this.sanitizer.bypassSecurityTrustResourceUrl(`/assets/icons/${icon}.svg`)
+      );
+    }
+  }
+}
