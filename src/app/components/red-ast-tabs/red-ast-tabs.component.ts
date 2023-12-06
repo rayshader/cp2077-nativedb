675059594624618222b6bd3aba4a6aaaa35d2c69
Louis Poirier <contact@louispoirier.io>
2023-12-06 21:55:34 +0100

    feat: disable empty tabs in <red-ast-tabs>.
    Refactor to list tabs with an Observable instead of tab's nodes.

--- src/app/components/red-ast-tabs/red-ast-tabs.component.ts
+++ src/app/components/red-ast-tabs/red-ast-tabs.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {MatTabsModule} from "@angular/material/tabs";
-import {Observable} from "rxjs";
+import {combineLatest, map, Observable} from "rxjs";
 import {MatIconModule} from "@angular/material/icon";
 import {AsyncPipe, NgTemplateOutlet} from "@angular/common";
 import {RouterLink} from "@angular/router";
@@ -15,7 +15,7 @@ interface TabItem {
   readonly uri: string;
   readonly icon: string;
   readonly alt: string;
-  readonly nodes$: Observable<RedNode[]>;
+  readonly nodes: RedNode[];
 }
 
 @Component({
@@ -33,16 +33,26 @@ interface TabItem {
 })
 export class RedAstTabsComponent {
 
-  readonly tabs: TabItem[];
+  readonly tabs: Observable<TabItem[]>;
 
   constructor(searchService: SearchService) {
-    this.tabs = [
-      {uri: 'e', icon: 'enum', alt: 'Enums', nodes$: searchService.enums$},
-      {uri: 'b', icon: 'bitfield', alt: 'Bitfields', nodes$: searchService.bitfields$},
-      {uri: 'c', icon: 'class', alt: 'Classes', nodes$: searchService.classes$},
-      {uri: 's', icon: 'struct', alt: 'Structs', nodes$: searchService.structs$},
-      {uri: 'f', icon: 'function', alt: 'Global functions', nodes$: searchService.functions$},
-    ];
+    this.tabs = combineLatest([
+      searchService.enums$,
+      searchService.bitfields$,
+      searchService.classes$,
+      searchService.structs$,
+      searchService.functions$,
+    ]).pipe(
+      map(([enums, bitfields, classes, structs, functions]) => {
+        return [
+          {uri: 'e', icon: 'enum', alt: 'Enums', nodes: enums},
+          {uri: 'b', icon: 'bitfield', alt: 'Bitfields', nodes: bitfields},
+          {uri: 'c', icon: 'class', alt: 'Classes', nodes: classes},
+          {uri: 's', icon: 'struct', alt: 'Structs', nodes: structs},
+          {uri: 'f', icon: 'function', alt: 'Global functions', nodes: functions},
+        ];
+      })
+    );
   }
 
 }
