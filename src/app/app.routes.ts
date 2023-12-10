f15df4885fba156765b2bed412b6f331cd2018f9
Louis Poirier <contact@louispoirier.io>
2023-12-10 17:20:35 +0100

    perf: split build output in chunks.
    Use component lazy-loading in router to create multiple chunks.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,22 @@
 import {Routes} from '@angular/router';
 import {ReadmeComponent} from "./pages/readme/readme.component";
-import {EnumComponent} from "./pages/enum/enum.component";
-import {BitfieldComponent} from "./pages/bitfield/bitfield.component";
-import {FunctionComponent} from "./pages/function/function.component";
 import {RedNodeKind} from "../shared/red-ast/red-node.ast";
-import {ObjectComponent} from "./pages/object/object.component";
-import {SettingsComponent} from "./pages/settings/settings.component";
-import {FunctionsComponent} from "./pages/functions/functions.component";
 
 export const routes: Routes = [
   {path: '', component: ReadmeComponent},
-  {path: 'settings', component: SettingsComponent},
-  {path: 'e/:id', component: EnumComponent},
-  {path: 'b/:id', component: BitfieldComponent},
-  {path: 'c/:id', component: ObjectComponent, data: {kind: RedNodeKind.class}},
-  {path: 's/:id', component: ObjectComponent, data: {kind: RedNodeKind.struct}},
-  {path: 'f', component: FunctionsComponent},
-  {path: 'f/:id', component: FunctionComponent},
+  {path: 'settings', loadComponent: () => import('./pages/settings/settings.component').then(c => c.SettingsComponent)},
+  {path: 'e/:id', loadComponent: () => import('./pages/enum/enum.component').then(c => c.EnumComponent)},
+  {path: 'b/:id', loadComponent: () => import('./pages/bitfield/bitfield.component').then(c => c.BitfieldComponent)},
+  {
+    path: 'c/:id',
+    loadComponent: () => import('./pages/object/object.component').then(c => c.ObjectComponent),
+    data: {kind: RedNodeKind.class}
+  },
+  {
+    path: 's/:id',
+    loadComponent: () => import('./pages/object/object.component').then(c => c.ObjectComponent),
+    data: {kind: RedNodeKind.struct}
+  },
+  {path: 'f', loadComponent: () => import('./pages/functions/functions.component').then(c => c.FunctionsComponent)},
+  {path: 'f/:id', loadComponent: () => import('./pages/function/function.component').then(c => c.FunctionComponent)},
 ];
