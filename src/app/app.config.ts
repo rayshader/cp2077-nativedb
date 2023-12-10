1b08e9409e4768570844b3e481b2c6bccec8afe0
Louis Poirier <contact@louispoirier.io>
2023-12-10 17:20:58 +0100

    style: organize imports in 'app.config'.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,17 @@
 import {ApplicationConfig, isDevMode} from '@angular/core';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
-
-import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideHttpClient} from "@angular/common/http";
 import {provideServiceWorker} from '@angular/service-worker';
 
+import {routes} from './app.routes';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(
+      routes,
+      withComponentInputBinding()
+    ),
     provideAnimations(),
     provideHttpClient(),
     provideServiceWorker('ngsw-worker.js', {
