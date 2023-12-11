1975aa3e92a82a114c36f04be1a501e9edf7840e
Louis Poirier <contact@louispoirier.io>
2023-12-12 00:12:21 +0100

    feat: add feature to resize <ndb-tabs> component.
    Save/load width value in/from local storage.
    Click and slide to resize the component within predefined limits (min 320px, max 50% of viewport's width).
    Double-click to restore default size.

--- src/app/components/ndb-tabs/ndb-tabs.component.ts
+++ src/app/components/ndb-tabs/ndb-tabs.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, Renderer2} from '@angular/core';
 import {MatTabsModule} from "@angular/material/tabs";
-import {combineLatest, map, Observable} from "rxjs";
+import {combineLatest, map, Observable, take} from "rxjs";
 import {MatIconModule} from "@angular/material/icon";
 import {AsyncPipe, NgTemplateOutlet} from "@angular/common";
 import {RouterLink} from "@angular/router";
@@ -8,6 +8,7 @@ import {SearchService} from "../../../shared/services/search.service";
 import {CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport} from "@angular/cdk/scrolling";
 import {MatDividerModule} from "@angular/material/divider";
 import {SettingsService} from "../../../shared/services/settings.service";
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 export interface TabItemNode {
   readonly id: number;
@@ -50,8 +51,14 @@ export class NDBTabsComponent {
     {icon: 'bitfield', alt: 'Bitfields', nodes: []},
   ];
 
-  constructor(searchService: SearchService,
-              settingsService: SettingsService) {
+  width: string = '320px';
+
+  private isResizing: boolean = false;
+
+  constructor(private readonly renderer: Renderer2,
+              private readonly settingsService: SettingsService,
+              searchService: SearchService) {
+    this.settingsService.tabsWidth$.pipe(take(1), takeUntilDestroyed()).subscribe(this.onWidthLoaded.bind(this));
     this.tabs$ = combineLatest([
       searchService.enums$,
       searchService.bitfields$,
@@ -92,4 +99,42 @@ export class NDBTabsComponent {
       })
     );
   }
+
+  protected onStartResizing(): void {
+    this.isResizing = true;
+    this.renderer.setStyle(document.body, 'user-select', 'none');
+    this.renderer.setStyle(document.body, 'cursor', 'col-resize');
+  }
+
+  @HostListener('window:mousemove', ['$event'])
+  protected onResizing(event: MouseEvent): void {
+    if (!this.isResizing) {
+      return;
+    }
+    if (event.x < 320 || event.x > document.body.clientWidth / 2) {
+      return;
+    }
+    this.width = `${event.x}px`;
+  }
+
+  @HostListener('window:mouseup')
+  protected onStopResizing(): void {
+    if (!this.isResizing) {
+      return;
+    }
+    this.renderer.removeStyle(document.body, 'user-select');
+    this.renderer.removeStyle(document.body, 'cursor');
+    this.settingsService.updateTabsWidth(parseInt(this.width));
+    this.isResizing = false;
+  }
+
+  protected onRestoreDefault(): void {
+    this.width = '320px';
+    this.settingsService.updateTabsWidth(320);
+  }
+
+  private onWidthLoaded(width: number): void {
+    this.width = `${width}px`;
+  }
+
 }
--- src/shared/services/settings.service.ts
+++ src/shared/services/settings.service.ts
@@ -8,6 +8,7 @@ export interface Settings {
   readonly highlightEmptyObject: boolean;
   readonly showEmptyAccordion: boolean;
   readonly mergeObject: boolean;
+  readonly tabsWidth: number;
   readonly clipboardSyntax: CodeSyntax;
   readonly codeSyntax: CodeSyntax;
 }
@@ -26,43 +27,41 @@ export enum CodeSyntax {
 export class SettingsService {
 
   private readonly ignoreDuplicateSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  private readonly scrollBehaviorSubject: BehaviorSubject<PageScrollBehavior> = new BehaviorSubject<PageScrollBehavior>('smooth');
-  private readonly highlightEmptyObjectSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  private readonly showEmptyAccordionSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private readonly mergeObjectSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  private readonly clipboardSubject: BehaviorSubject<CodeSyntax> = new BehaviorSubject<CodeSyntax>(CodeSyntax.redscript);
-  private readonly codeSubject: BehaviorSubject<CodeSyntax> = new BehaviorSubject<CodeSyntax>(CodeSyntax.redscript);
-
   /**
    * Whether duplicate operators/casts in global functions should be ignored (hidden)?
    */
   readonly ignoreDuplicate$: Observable<boolean> = this.ignoreDuplicateSubject.asObservable();
-
+  private readonly scrollBehaviorSubject: BehaviorSubject<PageScrollBehavior> = new BehaviorSubject<PageScrollBehavior>('smooth');
   /**
    * Which scrolling behavior should be used when navigating to a new page?
    */
   readonly scrollBehavior$: Observable<PageScrollBehavior> = this.scrollBehaviorSubject.asObservable();
-
+  private readonly highlightEmptyObjectSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   /**
    * Whether empty class/struct should be highlighted?
    */
   readonly highlightEmptyObject$: Observable<boolean> = this.highlightEmptyObjectSubject.asObservable();
-
+  private readonly showEmptyAccordionSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   /**
    * Whether empty accordions of an object should be visible?
    */
   readonly showEmptyAccordion$: Observable<boolean> = this.showEmptyAccordionSubject.asObservable();
-
+  private readonly mergeObjectSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   /**
    * Whether classes and structs should be listed in one tab?
    */
   readonly mergeObject$: Observable<boolean> = this.mergeObjectSubject.asObservable();
-
+  private readonly tabsWidthSubject: BehaviorSubject<number> = new BehaviorSubject<number>(320);
+  /**
+   * Width of the <ndb-tabs> panel (in pixels).
+   */
+  readonly tabsWidth$: Observable<number> = this.tabsWidthSubject.asObservable();
+  private readonly clipboardSubject: BehaviorSubject<CodeSyntax> = new BehaviorSubject<CodeSyntax>(CodeSyntax.redscript);
   /**
    * Which code syntax must be used when copying code to the clipboard?
    */
   readonly clipboard$: Observable<CodeSyntax> = this.clipboardSubject.asObservable();
-
+  private readonly codeSubject: BehaviorSubject<CodeSyntax> = new BehaviorSubject<CodeSyntax>(CodeSyntax.redscript);
   /**
    * Which code syntax must be used when formatting functions and properties?
    */
@@ -74,6 +73,7 @@ export class SettingsService {
     const highlightEmptyObject: boolean = (localStorage.getItem('highlight-empty-object') ?? 'true') === 'true';
     const showEmptyAccordion: boolean = (localStorage.getItem('show-empty-accordion') ?? 'false') === 'true';
     const mergeObject: boolean = (localStorage.getItem('merge-object') ?? 'false') === 'true';
+    const tabsWidth: number = +(localStorage.getItem('tabs-width') ?? '320');
     const clipboard: string = localStorage.getItem('clipboard-syntax') ?? CodeSyntax.redscript.toString();
     const code: string = localStorage.getItem('code-syntax') ?? CodeSyntax.redscript.toString();
 
@@ -82,6 +82,7 @@ export class SettingsService {
     this.highlightEmptyObjectSubject.next(highlightEmptyObject);
     this.showEmptyAccordionSubject.next(showEmptyAccordion);
     this.mergeObjectSubject.next(mergeObject);
+    this.tabsWidthSubject.next(tabsWidth);
     this.clipboardSubject.next(+clipboard);
     this.codeSubject.next(+code);
   }
@@ -93,6 +94,7 @@ export class SettingsService {
       this.highlightEmptyObject$,
       this.showEmptyAccordion$,
       this.mergeObject$,
+      this.tabsWidth$,
       this.clipboard$,
       this.code$
     ])
@@ -103,6 +105,7 @@ export class SettingsService {
                highlightEmptyObject,
                showEmptyAccordion,
                mergeObject,
+               tabsWidth,
                clipboard,
                code
              ]) => {
@@ -112,6 +115,7 @@ export class SettingsService {
             highlightEmptyObject: highlightEmptyObject,
             showEmptyAccordion: showEmptyAccordion,
             mergeObject: mergeObject,
+            tabsWidth: tabsWidth,
             clipboardSyntax: clipboard,
             codeSyntax: code
           };
@@ -142,6 +146,11 @@ export class SettingsService {
   updateMergeObject(state: boolean): void {
     localStorage.setItem('merge-object', `${state}`);
     this.mergeObjectSubject.next(state);
+  }
+
+  updateTabsWidth(width: number): void {
+    localStorage.setItem('tabs-width', width.toString());
+    this.tabsWidthSubject.next(width);
   }
 
   updateClipboard(syntax: CodeSyntax): void {
