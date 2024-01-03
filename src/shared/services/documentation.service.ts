6b4067705e114e52a1de6f4d5d9523b614a50049
Louis Poirier <contact@louispoirier.io>
2024-01-03 23:27:28 +0100

    feat: implementing merge operations when importing documentation.
    Generate a tree of changes between documentation of the browser and documentation of an imported file. Generate operations to merge classes/structs, bodies and members with add/update/delete actions.

--- src/shared/services/documentation.service.ts
+++ src/shared/services/documentation.service.ts
@@ -14,6 +14,43 @@ export interface MemberDocumentation {
   body: string;
 }
 
+export enum MergeOperation {
+  none,
+  add,
+  update,
+  delete
+}
+
+export enum MergeFrom {
+  browser,
+  file
+}
+
+export interface ClassMergeOperation {
+  readonly id: number;
+  readonly operation: MergeOperation;
+  readonly body?: BodyMergeOperation;
+  readonly members?: MemberMergeOperation[];
+}
+
+export interface BodyMergeOperation {
+  readonly id: number;
+  readonly operation: MergeOperation;
+  readonly browser?: string;
+  readonly file?: string;
+
+  from?: MergeFrom;
+}
+
+export interface MemberMergeOperation {
+  readonly id: number;
+  readonly operation: MergeOperation;
+  readonly browser?: string;
+  readonly file?: string;
+
+  from?: MergeFrom;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -68,8 +105,128 @@ export class DocumentationService {
     return this.classRepository.update(documentation);
   }
 
+  merge(file: ClassDocumentation[]): Observable<ClassMergeOperation[]> {
+    return this.getAll().pipe(
+      map((browser: ClassDocumentation[]) => {
+        const operations: ClassMergeOperation[] = [];
+
+        // Detect delete / update operations
+        browser.forEach((browserObject) => {
+          const fileObject: ClassDocumentation | undefined = file.find((item) => item.id === browserObject.id);
+
+          if (!fileObject) {
+            operations.push({
+              id: browserObject.id,
+              operation: MergeOperation.delete,
+              body: {
+                id: browserObject.id,
+                operation: MergeOperation.delete,
+                browser: browserObject.body
+              },
+              members: browserObject.functions?.map((member) => <MemberMergeOperation>{
+                id: member.id,
+                operation: MergeOperation.delete,
+                browser: member.body
+              })
+            });
+            return;
+          }
+          const body: BodyMergeOperation | undefined = this.mergeClassBody(browserObject, fileObject);
+          const members: MemberMergeOperation[] = this.mergeClassMembers(browserObject.functions ?? [], fileObject.functions ?? []);
+
+          if (body === undefined && members.length === 0) {
+            return;
+          }
+          operations.push({
+            id: browserObject.id,
+            operation: MergeOperation.none,
+            body: body,
+            members: members
+          });
+        });
+        // Detect add operations
+        file.forEach((fileObject) => {
+          const browserObject: ClassDocumentation | undefined = browser.find((item) => item.id === fileObject.id);
+
+          if (browserObject) {
+            return;
+          }
+          operations.push({
+            id: fileObject.id,
+            operation: MergeOperation.add,
+          });
+        });
+        return operations;
+      })
+    );
+  }
+
   private canDelete(documentation: ClassDocumentation): boolean {
     return !documentation.body && !documentation.functions;
+  }
+
+  private mergeClassBody(browser: ClassDocumentation, file: ClassDocumentation): BodyMergeOperation | undefined {
+    if (browser.body === file.body) {
+      return undefined;
+    }
+    let operation: MergeOperation;
+
+    if (browser.body && file.body) {
+      operation = MergeOperation.update;
+    } else if (browser.body && !file.body) {
+      operation = MergeOperation.delete;
+    } else {
+      operation = MergeOperation.add;
+    }
+    return {
+      id: browser.id,
+      operation: operation,
+      browser: browser.body,
+      file: file.body,
+      from: operation === MergeOperation.add ? MergeFrom.file : undefined
+    };
+  }
+
+  private mergeClassMembers(browser: MemberDocumentation[], file: MemberDocumentation[]): MemberMergeOperation[] {
+    const operations: MemberMergeOperation[] = [];
+
+    // Detect delete / update operations.
+    browser.forEach((browserMember) => {
+      const fileMember: MemberDocumentation | undefined = file.find((item) => item.id === browserMember.id);
+
+      if (!fileMember) {
+        operations.push({
+          id: browserMember.id,
+          operation: MergeOperation.delete,
+          browser: browserMember.body
+        });
+        return;
+      }
+      if (browserMember.body === fileMember.body) {
+        return;
+      }
+      operations.push({
+        id: browserMember.id,
+        operation: MergeOperation.update,
+        browser: browserMember.body,
+        file: fileMember.body
+      });
+    });
+    // Detect add operations
+    file.forEach((fileMember) => {
+      const browserMember: MemberDocumentation | undefined = file.find((item) => item.id === fileMember.id);
+
+      if (browserMember) {
+        return;
+      }
+      operations.push({
+        id: fileMember.id,
+        operation: MergeOperation.add,
+        file: fileMember.body,
+        from: MergeFrom.file
+      });
+    });
+    return operations;
   }
 
 }
