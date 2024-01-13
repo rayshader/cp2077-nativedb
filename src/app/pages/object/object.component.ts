c8dc77b5db8be86afd4e3be209d25f7fdea9d7eb
Louis Poirier <contact@louispoirier.io>
2024-01-14 00:06:55 +0100

    feat: add filter feature on badges of properties/functions.
    Let user filter lists of properties/functions of classes/structs.
    Let user reset filter.
    Show accordion's header of properties/functions as sticky when scrolling.

--- src/app/pages/object/object.component.ts
+++ src/app/pages/object/object.component.ts
@@ -6,7 +6,20 @@ import {MatIconModule} from "@angular/material/icon";
 import {PropertySpanComponent} from "../../components/spans/property-span/property-span.component";
 import {NDBAccordionItemComponent} from "../../components/ndb-accordion-item/ndb-accordion-item.component";
 import {TypeSpanComponent} from "../../components/spans/type-span/type-span.component";
-import {combineLatest, EMPTY, filter, first, map, Observable, of, OperatorFunction, pipe, switchMap} from "rxjs";
+import {
+  BehaviorSubject,
+  combineLatest,
+  EMPTY,
+  filter,
+  first,
+  map,
+  Observable,
+  of,
+  OperatorFunction,
+  pipe,
+  shareReplay,
+  switchMap
+} from "rxjs";
 import {RedDumpService} from "../../../shared/services/red-dump.service";
 import {RedNodeKind} from "../../../shared/red-ast/red-node.ast";
 import {ActivatedRoute} from "@angular/router";
@@ -51,6 +64,15 @@ interface ObjectData {
   readonly showFunctions: boolean;
 }
 
+interface BadgeFilterItem<T> {
+  isEnabled: boolean;
+
+  readonly icon: string;
+  readonly title: string;
+  readonly dataScope?: string;
+  readonly filter: (node: T) => boolean;
+}
+
 @Component({
   selector: 'object',
   standalone: true,
@@ -74,6 +96,52 @@ interface ObjectData {
 })
 export class ObjectComponent {
 
+  readonly badgesProperty: BadgeFilterItem<RedPropertyAst>[] = [
+    {isEnabled: true, icon: 'scope', title: 'public', filter: (prop) => prop.visibility === RedVisibilityDef.public},
+    {
+      isEnabled: true,
+      icon: 'scope',
+      title: 'protected',
+      dataScope: 'protected',
+      filter: (prop) => prop.visibility === RedVisibilityDef.protected
+    },
+    {
+      isEnabled: true,
+      icon: 'scope',
+      title: 'private',
+      dataScope: 'private',
+      filter: (prop) => prop.visibility === RedVisibilityDef.private
+    },
+
+    {isEnabled: true, icon: 'scope', title: 'persistent', filter: (prop) => prop.isPersistent}
+  ];
+  readonly badgesFunction: BadgeFilterItem<RedFunctionAst>[] = [
+    {isEnabled: true, icon: 'scope', title: 'public', filter: (func) => func.visibility === RedVisibilityDef.public},
+    {
+      isEnabled: true,
+      icon: 'scope',
+      title: 'protected',
+      dataScope: 'protected',
+      filter: (func) => func.visibility === RedVisibilityDef.protected
+    },
+    {
+      isEnabled: true,
+      icon: 'scope',
+      title: 'private',
+      dataScope: 'private',
+      filter: (func) => func.visibility === RedVisibilityDef.private
+    },
+
+    {isEnabled: true, icon: 'native', title: 'native', filter: (func) => func.isNative},
+    {isEnabled: true, icon: 'static', title: 'static', filter: (func) => func.isStatic},
+    {isEnabled: true, icon: 'final', title: 'final', filter: (func) => func.isFinal},
+    {isEnabled: true, icon: 'timer', title: 'threadsafe', filter: (func) => func.isThreadSafe},
+    {isEnabled: true, icon: 'callback', title: 'callback', filter: (func) => func.isCallback},
+    {isEnabled: true, icon: 'const', title: 'const', filter: (func) => func.isConst},
+    {isEnabled: true, icon: 'quest', title: 'quest', filter: (func) => func.isQuest},
+    {isEnabled: true, icon: 'timer', title: 'timer', filter: (func) => func.isTimer}
+  ];
+
   data$: Observable<ObjectData | undefined> = EMPTY;
   showDocumentation: boolean = false;
 
@@ -86,6 +154,9 @@ export class ObjectComponent {
   protected readonly importOnlyOrigin: RedOriginDef = RedOriginDef.importOnly;
 
   protected readonly cyrb53 = cyrb53;
+
+  private readonly filterBadgesSubject: BehaviorSubject<void> = new BehaviorSubject<void>(undefined);
+  private readonly filterFunctions$: Observable<void> = this.filterBadgesSubject.asObservable();
 
   constructor(private readonly dumpService: RedDumpService,
               private readonly documentationService: DocumentationService,
@@ -105,6 +176,8 @@ export class ObjectComponent {
       this.pageService.restoreScroll();
     }
     this.recentVisitService.pushLastVisit(+id);
+    this.resetPropertiesFilter();
+    this.resetFunctionsFilter();
     const object$ = of(this.kind).pipe(
       switchMap((kind) => {
         if (kind === RedNodeKind.class) {
@@ -114,11 +187,12 @@ export class ObjectComponent {
         }
         return EMPTY;
       }),
-      map((object) => object as RedClassAst)
+      map((object) => object as RedClassAst),
+      shareReplay()
     );
-    const parents$ = object$.pipe(this.getParents());
-    const children$ = object$.pipe(this.getChildren());
-    const documentation$ = object$.pipe(this.getDocumentation());
+    const parents$ = object$.pipe(this.getParents(), shareReplay());
+    const children$ = object$.pipe(this.getChildren(), shareReplay());
+    const documentation$ = object$.pipe(this.getDocumentation(), shareReplay());
 
     combineLatest([
       this.settingsService.showDocumentation$,
@@ -132,6 +206,7 @@ export class ObjectComponent {
       this.dumpService.badges$,
       this.settingsService.showEmptyAccordion$,
       this.responsiveService.mobile$,
+      this.filterFunctions$
     ]).pipe(
       map(([
              object,
@@ -142,10 +217,16 @@ export class ObjectComponent {
              showEmptyAccordion,
              isMobile
            ]) => {
-        const properties = object.properties;
-        const functions = object.functions;
+        let properties = object.properties;
+        let functions = object.functions;
 
+        if (this.isPropertiesFiltered) {
+          properties = properties.filter(this.hasPropertyFlag.bind(this));
+        }
         properties.sort(RedPropertyAst.sort);
+        if (this.isFunctionsFiltered) {
+          functions = functions.filter(this.hasFunctionFlag.bind(this));
+        }
         functions.sort(RedFunctionAst.sort);
         this.app.isStable.pipe(
           filter((stable: boolean) => stable),
@@ -167,15 +248,73 @@ export class ObjectComponent {
           hasBodyDocumentation: documentation.body !== undefined,
           showParents: parents.length > 0 || showEmptyAccordion,
           showChildren: children.length > 0 || showEmptyAccordion,
-          showProperties: properties.length > 0 || showEmptyAccordion,
-          showFunctions: functions.length > 0 || showEmptyAccordion,
+          showProperties: properties.length > 0 || showEmptyAccordion || this.isPropertiesFiltered,
+          showFunctions: functions.length > 0 || showEmptyAccordion || this.isFunctionsFiltered,
         };
       })
     );
   }
 
+  protected get disableResetFilterProperties(): boolean {
+    return this.badgesProperty.filter((badge) => badge.isEnabled).length === this.badgesProperty.length;
+  }
+
+  protected get disableResetFilterFunctions(): boolean {
+    return this.badgesFunction.filter((badge) => badge.isEnabled).length === this.badgesFunction.length;
+  }
+
+  protected get isFunctionsFiltered(): boolean {
+    return this.badgesFunction.filter((badge) => badge.isEnabled).length === 1;
+  }
+
+  protected get isPropertiesFiltered(): boolean {
+    return this.badgesProperty.filter((badge) => badge.isEnabled).length === 1;
+  }
+
   toggleDocumentation(): void {
     this.showDocumentation = !this.showDocumentation;
+  }
+
+  togglePropertyFilter(badge: BadgeFilterItem<RedPropertyAst>): void {
+    this.badgesProperty.forEach((badge) => badge.isEnabled = false);
+    badge.isEnabled = !badge.isEnabled;
+    this.filterBadgesSubject.next();
+  }
+
+  toggleFunctionFilter(badge: BadgeFilterItem<RedFunctionAst>): void {
+    this.badgesFunction.forEach((badge) => badge.isEnabled = false);
+    badge.isEnabled = !badge.isEnabled;
+    this.filterBadgesSubject.next();
+  }
+
+  resetPropertiesFilter(): void {
+    this.badgesProperty.forEach((badge) => badge.isEnabled = true);
+    this.filterBadgesSubject.next();
+  }
+
+  resetFunctionsFilter(): void {
+    this.badgesFunction.forEach((badge) => badge.isEnabled = true);
+    this.filterBadgesSubject.next();
+  }
+
+  private hasPropertyFlag(prop: RedPropertyAst): boolean {
+    const badges: BadgeFilterItem<RedPropertyAst>[] = this.badgesProperty.filter((badge) => badge.isEnabled);
+    let match: boolean = false;
+
+    for (const badge of badges) {
+      match ||= badge.filter(prop);
+    }
+    return match;
+  }
+
+  private hasFunctionFlag(func: RedFunctionAst): boolean {
+    const badges: BadgeFilterItem<RedFunctionAst>[] = this.badgesFunction.filter((badge) => badge.isEnabled);
+    let match: boolean = false;
+
+    for (const badge of badges) {
+      match ||= badge.filter(func);
+    }
+    return match;
   }
 
   private onShowDocumentation([state, documentation]: [boolean, ClassDocumentation]): void {
