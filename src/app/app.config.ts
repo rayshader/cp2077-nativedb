9852e20be3b6087ef97b52e7dfcc45463f81c42f
Louis Poirier <contact@louispoirier.io>
2024-01-15 23:40:55 +0100

    perf: improve webapp loading.
    Lazy-load Angular animations module.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import {ApplicationConfig, isDevMode} from '@angular/core';
 import {PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading} from '@angular/router';
-import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideHttpClient} from "@angular/common/http";
 import {provideServiceWorker} from '@angular/service-worker';
 
 import {routes} from './app.routes';
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +13,7 @@ export const appConfig: ApplicationConfig = {
       withComponentInputBinding(),
       withPreloading(PreloadAllModules)
     ),
-    provideAnimations(),
+    provideAnimationsAsync(),
     provideHttpClient(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
