15940a5e4239b1bf335db56177a06e57d0268dc5
Louis Poirier <contact@louispoirier.io>
2024-02-01 17:59:39 +0100

    build: install brotli-wasm.
    Add gulp task to copy .wasm file in src/.
    Add .wasm file in angular project to be bundled as an asset.

--- gulp.mjs
+++ gulp.mjs
@@ -2,6 +2,11 @@ import gulp from 'gulp';
 import gzip from 'gulp-gzip';
 import brotli from 'gulp-brotli';
 
+gulp.task('install-wasm', () => {
+  return gulp.src('./node_modules/brotli-wasm/pkg.web/*.wasm')
+    .pipe(gulp.dest('./src'));
+});
+
 gulp.task('compress-gzip', () => {
   return gulp.src(['./dist/**/*.*'])
     .pipe(gzip())
