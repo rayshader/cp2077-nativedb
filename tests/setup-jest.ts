456c00e59894fb2cee462cc7b5a8b3bce9ee3dfe
Louis Poirier <contact@louispoirier.io>
2026-08-16 13:41:34 +0200

    test: add missing console stub

--- tests/setup-jest.ts
+++ tests/setup-jest.ts
@@ -1,3 +1,12 @@
 import {setupZoneTestEnv} from "jest-preset-angular/setup-env/zone/index.js";
 
 setupZoneTestEnv();
+
+console = {
+  ...console,
+  log: jest.fn(),
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
